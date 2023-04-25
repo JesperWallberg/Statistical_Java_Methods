12bdda4725ce86a154c3178b381135a21df4930c
JesperWallberg <carl.jesperwallberg@gmail.com>
2023-04-25 22:01:06 +0200

    + Binominal Distribution + Hypergemetric distribution + Poisson distribution

--- src/Stat.java
+++ src/Stat.java
@@ -1,4 +1,19 @@
 public class Stat {
+    /* Binomnial distribution */
+    public static double binominalDist(double k, double n, double p){
+        return nCr(n, k) * Math.pow(p, k) * Math.pow((1 - p), (n - k));
+    }
+
+    /* Hypergeometric distribution */
+    public static double hyperDist(double k, double N, double n, double p){
+        return nCr((N * p), k) * nCr(N * (1 - p), (n - k)) / nCr(N, n);
+    }
+
+    /* Poisson distribution */
+    public static double poissonDist(double k, double u){
+        return (Math.pow(u, k) / factorial(k)) * Math.exp(-u);
+    }
+
     /* Combinations */
     public static double nCr(double a, double b){
         return factorial(a) / (factorial(b) * factorial(a - b));

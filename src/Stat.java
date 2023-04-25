552bbf95aeccfb3101713192256cc176419e52f8
Jesper Wallberg <10283459+JesperWallberg@users.noreply.github.com>
2023-04-25 21:27:23 +0200

    Update Stat.java

--- src/Stat.java
+++ src/Stat.java
@@ -1,4 +1,14 @@
 public class Stat {
+    
+    /* Factorial of a */
+    public static double factorial(double a){
+        double answer = 1;
+        for(double i = a; i > 0; i--){
+            answer *= i;
+        }
+        return answer;
+    }
+    
     /* Combinations */
     public static double nCr(double a, double b){
         return factorial(a) / (factorial(b) * factorial(a - b));
@@ -7,14 +17,5 @@ public class Stat {
     /* Permutations */
     public static double nPr(double a, double b){
         return factorial(a) / factorial(a - b);
-    }
-
-    /* Factorial of a */
-    public static double factorial(double a){
-        double answer = 1;
-        for(double i = a; i > 0; i--){
-            answer *= i;
-        }
-        return answer;
     }
 }
